c3a182d2e9d3b316bd03596843b3164cd9dab1ac
TrueTrizzy <Tycoleman523@gmail.com>
2019-12-05 18:09:55 -0600

    Add project files.

---
+++ Test stuff/My_app.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Test_stuff
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Website_link = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.All_links = new System.Windows.Forms.Button();
+            this.Youtube = new System.Windows.Forms.Button();
+            this.Steam = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Mcserver_pannel = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Website_link
+            // 
+            this.Website_link.Location = new System.Drawing.Point(3, 3);
+            this.Website_link.Name = "Website_link";
+            this.Website_link.Size = new System.Drawing.Size(75, 23);
+            this.Website_link.TabIndex = 0;
+            this.Website_link.Text = "Website";
+            this.Website_link.UseVisualStyleBackColor = true;
+            this.Website_link.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.All_links);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(81, 88);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // All_links
+            // 
+            this.All_links.Location = new System.Drawing.Point(3, 3);
+            this.All_links.Name = "All_links";
+            this.All_links.Size = new System.Drawing.Size(75, 23);
+            this.All_links.TabIndex = 2;
+            this.All_links.Text = "Info";
+            this.All_links.UseVisualStyleBackColor = true;
+            this.All_links.Click += new System.EventHandler(this.All_links_Click);
+            // 
+            // Youtube
+            // 
+            this.Youtube.Location = new System.Drawing.Point(3, 32);
+            this.Youtube.Name = "Youtube";
+            this.Youtube.Size = new System.Drawing.Size(75, 23);
+            this.Youtube.TabIndex = 2;
+            this.Youtube.Text = "Youtube";
+            this.Youtube.UseVisualStyleBackColor = true;
+            this.Youtube.Click += new System.EventHandler(this.Youtube_Click);
+            // 
+            // Steam
+            // 
+            this.Steam.Location = new System.Drawing.Point(3, 61);
+            this.Steam.Name = "Steam";
+            this.Steam.Size = new System.Drawing.Size(75, 23);
+            this.Steam.TabIndex = 3;
+            this.Steam.Text = "Steam";
+            this.Steam.UseVisualStyleBackColor = true;
+            this.Steam.Click += new System.EventHandler(this.Steam_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(846, 519);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(119, 44);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Quit App";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Discord : TrueTrizz#4566";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(0, 494);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 73);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(167, 67);
+            this.panel3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(10, 10);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Mcserver_pannel
+            // 
+            this.Mcserver_pannel.Location = new System.Drawing.Point(3, 3);
+            this.Mcserver_pannel.Name = "Mcserver_pannel";
+            this.Mcserver_pannel.Size = new System.Drawing.Size(128, 26);
+            this.Mcserver_pannel.TabIndex = 7;
+            this.Mcserver_pannel.Text = "MC_server_Managment";
+            this.Mcserver_pannel.UseVisualStyleBackColor = true;
+            this.Mcserver_pannel.Click += new System.EventHandler(this.Mcserver_pannel_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel2);
+            this.panel4.Controls.Add(this.Website_link);
+            this.panel4.Controls.Add(this.Youtube);
+            this.panel4.Controls.Add(this.Steam);
+            this.panel4.Location = new System.Drawing.Point(12, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(81, 88);
+            this.panel4.TabIndex = 8;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.Mcserver_pannel);
+            this.panel5.Location = new System.Drawing.Point(179, 536);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(134, 31);
+            this.panel5.TabIndex = 9;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(179, 533);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(137, 34);
+            this.panel6.TabIndex = 10;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
+            // 
+            // main
+            // 
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(994, 568);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test App";
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button All_links;
+        public System.Windows.Forms.Button Website_link;
+        private System.Windows.Forms.Button Youtube;
+        private System.Windows.Forms.Button Steam;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button Mcserver_pannel;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel6;
+    }
+}
+
---
+++ Test stuff/My_app.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Test_stuff
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://tycoleman523.wixsite.com/truegaming");
+            SendToBack();
+            panel2.Show();
+        }
+
+        private void All_links_Click(object sender, EventArgs e)
+        {
+            panel2.Hide();
+            
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Youtube_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.youtube.com/channel/UCb26GSI8QUzlUeCKivAscDA?view_as=subscriber");
+            SendToBack();
+            panel2.Show();
+        }
+
+        private void Steam_Click(object sender,EventArgs e)
+        {
+            Process.Start("https://steamcommunity.com/id/TrueTrizz/");
+            SendToBack();
+            panel2.Show();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mcserver_pannel_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://107.3.253.110:8081/");
+            SendToBack();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel5.Show();
+            BringToFront();
+            panel6.Hide();
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+            BringToFront();
+            
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+            SendToBack();
+        }
+    }
+}
